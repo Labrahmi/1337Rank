f74cf95f5efd3710a1ec89700da8e3632e2bf330
Labrahmi <yousseflabrahmi8@gmail.com>
2024-06-23 18:35:24 -0500

    fix some errors

--- src/pages/Callback.tsx
+++ src/pages/Callback.tsx
@@ -1,6 +1,5 @@
-import { useState, useEffect } from 'react'
+import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { Link } from 'react-router-dom'
 import { useLocation } from 'react-router-dom'
 import '../App.css'
 
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,3 @@
-import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 import '../App.css'
 
